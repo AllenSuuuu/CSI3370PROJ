e38b9e95ba67b6f24a03f11631c773a08b4ad788
zeleney30 <michaelzeleney@gmail.com>
2023-11-20 18:22:21 -0500

    Add WASD standard movement
    S doesnt do anything (theres no down movement function). Space also works to jump as well.

--- Player/Player.gd
+++ Player/Player.gd
@@ -14,12 +14,13 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if (Input.is_action_just_pressed("ui_up") || Input.is_action_just_pressed("W") || Input.is_action_just_pressed("Space")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	direction += Input.get_axis("A", "D")
 
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
