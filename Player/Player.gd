ce0701123805412df860f7876206a2525f63c318
zeleney30 <michaelzeleney@gmail.com>
2023-11-22 15:47:58 -0500

    Double Jump

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,14 +9,25 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var anim = get_node("AnimationPlayer")
 
+var canDoubleJump : bool = true
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	# Handle Jump.
-	if (Input.is_action_just_pressed("ui_up") || Input.is_action_just_pressed("W") || Input.is_action_just_pressed("Space")) and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anim.play("Jump")
+	if (is_on_floor()):
+		canDoubleJump = true
+		if (Input.is_action_just_pressed("ui_up") || Input.is_action_just_pressed("W") || Input.is_action_just_pressed("Space")):
+			velocity.y = JUMP_VELOCITY
+			anim.play("Jump")
+	else:
+		if (canDoubleJump):
+			if (Input.is_action_just_pressed("ui_up") || Input.is_action_just_pressed("W") || Input.is_action_just_pressed("Space")):
+				velocity.y = JUMP_VELOCITY
+				anim.play("Jump")
+				canDoubleJump = false
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -44,3 +55,5 @@ func _physics_process(delta):
 		get_tree().change_scene_to_file("res://Scenes/main.tscn")
 		Game.playerHP = 10
 		Game.Gold = 0
+	
+	pass
