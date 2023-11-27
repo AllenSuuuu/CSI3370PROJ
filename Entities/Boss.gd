fd13448085d66b89d0337fc12d4e3b7b56d84187
zeleney30 <michaelzeleney@gmail.com>
2023-11-26 21:09:45 -0500

    Beginning Boss

---
+++ Entities/Boss.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+
+var SPEED = 150
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player
+var controller
+var chase = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	controller = get_node("../../..")
+	player = get_node("../../Player/Player")
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#Gravity for Frog
+	velocity.y += gravity * delta
+	if chase == true:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Run")
+		player = get_node("../../Player/Player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+		velocity.x = direction.x * SPEED
+	else:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Idle")
+		velocity.x = 0
+	move_and_slide()
+	
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		chase = true
+	
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
+		chase = false
+	
+	pass # Replace with function body.
